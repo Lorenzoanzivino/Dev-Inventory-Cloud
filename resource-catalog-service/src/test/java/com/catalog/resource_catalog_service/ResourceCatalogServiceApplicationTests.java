b757e32d63b1e3a2d7b0b2c84f53954a675084f6
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2026-04-07 16:03:53 +0200

    feat: completato flusso verticale per Resource (Entity, DTO, Mapper, Service, Controller)

---
+++ resource-catalog-service/src/test/java/com/catalog/resource_catalog_service/ResourceCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catalog.resource_catalog_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceCatalogServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
