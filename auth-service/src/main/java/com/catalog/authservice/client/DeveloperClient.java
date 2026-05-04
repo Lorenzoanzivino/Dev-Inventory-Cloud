0b975e730539a31baa5584fe70bb9ab22e98b254
Lorenzoanzivino <lorenzoanzivino1997@gmail.com>
2026-05-04 16:51:48 +0200

    feat: implement structured JSON logging across all services

--- auth-service/src/main/java/com/catalog/authservice/client/DeveloperClient.java
+++ auth-service/src/main/java/com/catalog/authservice/client/DeveloperClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 // Puntiamo direttamente all'URL configurato nel file properties/yaml
-@FeignClient(name = "developer-collection-service", url = "${services.developer-collection.url}")
+@FeignClient(name = "developer-client", url = "${services.developer-collection.url}")
 public interface DeveloperClient {
 
     @PostMapping("/api/v1/developers")
